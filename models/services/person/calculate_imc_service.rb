f7a2dc289630835925221334a2f26ba1dd1ea623
JuanCoutinho <JuanCoutinho>
2024-10-19 16:50:35 -0300

    [feat] simple services

--- models/services/person/calculate_imc_service.rb
+++ models/services/person/calculate_imc_service.rb
@@ -1,26 +1,10 @@
 module Services
   module Person 
     class CalculateImcService
-      def initialize(weight: nil, height: nil)
-        @weight = weight
-        @height = height
-      end
+      def self.call(weight:, height:)
+        return nil if weight.nil? || height.nil? || height.zero?
 
-      def call
-        run
-      end
-
-      def self.call(weight: nil, height: nil)
-        new(weight: weight, height: height).call
-      end
-
-      private 
-
-      def run 
-        return nil if @weight.nil? || @height.nil? || @height.zero?
-
-        imc = @weight / (@height * 2)
-        imc.round(2)
+        (weight / (height ** 2)).round(2)
       end
     end
   end
--- models/services/person/calculate_pam_service.rb
+++ models/services/person/calculate_pam_service.rb
@@ -1,27 +1,12 @@
 module Services
   module Person
-    class CalculatePamService 
-      def initialize(pas: nil, pad: nil)
-        @pas = pas
-        @pad = pad
-      end
-      
-      def call
-        run
-      end
+    class CalculatePamService
+      def self.call(pas:, pad:)
+        return nil if pas.nil? || pad.nil? || pad.zero?
 
-      def self.call(pas: nil, pad: nil)
-        new(pas: pas, pad: pad).call
-      end
-
-      private
-       
-      def run
-        return nil if @pas.nil? || @pad.nil?
-
-        pam = (@pas + 2 * @pad) / 3.0
+        pam = (pas + 2 * pad) / 3.0
         pam.round(2)
       end
-    end
+    end        
   end
 end
