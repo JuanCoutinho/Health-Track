3b928f9b461f39c1b92b656336df6158c2b24891
JuanCoutinho <JuanCoutinho>
2024-10-20 11:00:15 -0300

    [feat] add initializers

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require_relative 'initializer'
+require_relative 'config/initializers'
 
 class App
   def call
---
+++ config/initializers.rb
@@ -0,0 +1,19 @@
+# binding.irb
+require_relative '../models/modules/jsonable'
+require_relative '../models/modules/objectable'
+require_relative '../models/postgres_exporter'
+require_relative '../models/csv_exporter'
+require_relative '../models/record'
+
+base =  "#{File.dirname(__FILE__)}/.."
+
+models = "#{base}/models/**/*.rb"
+
+controllers = "#{base}/controllers/**/*.rb"
+
+views = "#{base}/views/**/*.rb"
+
+[views, models, controllers].each do |collection|
+  files = Dir[collection]
+  files.each {|file| require file }
+end
